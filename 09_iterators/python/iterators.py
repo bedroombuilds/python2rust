40ead27f1998ea6cc99534513ccb3278c7f9aed2
Michael Kefeder <m.kefeder@gmail.com>
2021-07-02 17:23:50 +0200

    code for 0x09 Iterators

---
+++ 09_iterators/python/iterators.py
@@ -0,0 +1,34 @@
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < 5:
+            self.count += 1
+            return self.count
+        raise StopIteration
+
+
+if __name__ == '__main__':
+    counter = Counter()
+    assert next(counter) == 1
+    assert next(counter) == 2
+    assert next(counter) == 3
+    assert next(counter) == 4
+    assert next(counter) == 5
+    try:
+        next(counter)
+    except StopIteration:
+        assert True
+
+    count1 = Counter()
+    count2 = Counter()
+    next(count2)
+    assert sum(filter(lambda x: x % 3 == 0, map(lambda t: t[0] * t[1], zip(count1, count2)))) == 18
+    count1 = Counter()
+    count2 = Counter()
+    next(count2)
+    assert sum((x for x in (a * b for a, b in zip(count1, count2)) if x % 3 == 0)) == 18
---
+++ 09_iterators/rust/iterators/src/lib.rs
@@ -0,0 +1,49 @@
+pub struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    pub fn new() -> Self {
+        Self { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new()
+            .zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(18, sum);
+    }
+}
